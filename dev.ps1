3e335aa07b2100a08f50c1dcfff3fd3d6cc3fe1c
Kurnia Jaya Teknik <dev@kurnia-jaya-teknik.com>
2026-05-22 14:41:16 +0700

    Add dev scripts

---
+++ dev.ps1
@@ -0,0 +1,3 @@
+$env:PATH = "C:\Program Files\nodejs;$env:PATH"
+Set-Location d:\Web\Company-profile-Fix
+& "C:\Program Files\nodejs\node.exe" "C:\Program Files\nodejs\node_modules\npm\bin\npm-cli.js" run dev
